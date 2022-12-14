859a09973e2dd81ed730e7ae9c6e17ad3703051e
alvarofmk <alvarofmk@gmail.com>
2022-12-14 10:38:43 +0100

    Commit inicial

---
+++ src/main/java/com/salesianostriana/kilo/KiloApplication.java
@@ -0,0 +1,13 @@
+package com.salesianostriana.kilo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KiloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KiloApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/salesianostriana/kilo/KiloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salesianostriana.kilo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KiloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
