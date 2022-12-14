adb1f74b2265f5ce57f12bee4c3658c669184442
Durbanban <carlosdurban91@gmail.com>
2022-12-14 18:55:20 +0100

    Detalles aportación

---
+++ src/main/java/com/salesianostriana/kilo/MainTest.java
@@ -0,0 +1,15 @@
+package com.salesianostriana.kilo;
+
+import com.salesianostriana.kilo.entities.Aportacion;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+
+@Component
+public class MainTest {
+
+    @PostConstruct
+    public void run() {
+        Aportacion a1 = Aportacion
+    }
+}
