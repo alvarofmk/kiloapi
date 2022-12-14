893faa63eb53459e2927483b4944e455e0674e28
Álvaro <alvarofmk@gmail.com>
2022-12-14 18:56:17 +0100

    Endpoint creado, falta documentación

---
+++ src/main/java/com/salesianostriana/kilo/repositories/CajaRepository.java
@@ -0,0 +1,9 @@
+package com.salesianostriana.kilo.repositories;
+
+import com.salesianostriana.kilo.entities.Caja;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CajaRepository extends JpaRepository<Caja, Long> {
+}
