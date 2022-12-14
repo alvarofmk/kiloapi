34732b910282328264a35a25209c413301b0f43b
Álvaro <alvarofmk@gmail.com>
2022-12-14 20:17:06 +0100

    Creado endpoint post caja, generada documentacion, y arreglada dependencia documentacion

--- src/main/java/com/salesianostriana/kilo/controllers/CajaController.java
+++ src/main/java/com/salesianostriana/kilo/controllers/CajaController.java
@@ -2,14 +2,21 @@ package com.salesianostriana.kilo.controllers;
 
 import com.fasterxml.jackson.annotation.JsonView;
 import com.salesianostriana.kilo.dtos.CajaResponseDTO;
+import com.salesianostriana.kilo.dtos.requests.CreateCajaDTO;
 import com.salesianostriana.kilo.entities.Caja;
 import com.salesianostriana.kilo.services.CajaService;
 import com.salesianostriana.kilo.views.View;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -20,6 +27,29 @@ public class CajaController {
 
     private final CajaService cajaService;
 
+    @Operation(summary = "Obtiene todas las cajas")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Cajas encontradas",
+                    content = { @Content(mediaType = "application/json",
+                            array = @ArraySchema(schema = @Schema(implementation = CajaResponseDTO.class)),
+                            examples = @ExampleObject(value = """
+                                    [
+                                        {
+                                            "id": 1,
+                                            "qr": "http://localhost:8080/caja/1",
+                                            "numCaja": 7,
+                                            "kilosTotales": 12.5
+                                        },
+                                        {
+                                            "id": 2,
+                                            "qr": "http://localhost:8080/caja/2",
+                                            "numCaja": 14,
+                                            "kilosTotales": 7.0
+                                        }
+                                    ]
+                                    """)) }),
+            @ApiResponse(responseCode = "404", description = "No se encuentra ninguna caja",
+                    content = @Content) })
     @GetMapping("/")
     @JsonView(View.CajaView.GenericResponseView.class)
     public ResponseEntity<List<CajaResponseDTO>> getAllCajas(){
@@ -31,6 +61,31 @@ public class CajaController {
                     .toList());
         else
             return ResponseEntity.notFound().build();
+    }
+
+    @Operation(summary = "Crea una nueva caja")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Caja creada con éxito",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Caja.class),
+                            examples = @ExampleObject(value = """
+                                    {
+                                        "id": 2,
+                                        "qr": "http://localhost:8080/caja/2",
+                                        "numCaja": 1,
+                                        "kilosTotales": 0.0,
+                                        "alimentos": [],
+                                        "destinatario": null
+                                    }
+                                    """)) }),
+            @ApiResponse(responseCode = "400", description = "Hay algún error en los datos",
+                    content = @Content) })
+    @PostMapping("/")
+    public ResponseEntity<Caja> createCaja(@RequestBody CreateCajaDTO createCajaDTO){
+        if (createCajaDTO.getNumCaja() <= 0)
+            return ResponseEntity.badRequest().build();
+        else
+            return ResponseEntity.status(HttpStatus.CREATED).body(cajaService.createCaja(createCajaDTO));
     }
 
 }
