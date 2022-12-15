7def798087a4774641258d7a4d01a715303833b8
Antonio Montero <antoniomonforma99@gmail.com>
2022-12-15 12:04:08 +0100

    Get All Clases done and DTO implementation

--- src/main/java/com/salesianostriana/kilo/controllers/ClaseController.java
+++ src/main/java/com/salesianostriana/kilo/controllers/ClaseController.java
@@ -5,8 +5,10 @@ import com.salesianostriana.kilo.entities.Clase;
 import com.salesianostriana.kilo.services.ClaseService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
@@ -18,6 +20,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
 import java.util.Optional;
 
 
@@ -39,7 +42,9 @@ public class ClaseController {
                         {
                             "id" = 123,
                             "nombre" = "2DAM",
-                            "tutor" = "Luis Miguel Lopez"
+                            "tutor" = "Luis Miguel Lopez",
+                            "numAportaciones" = 3,
+                            "totalKilos" = 12.6
                         }
                     """))
             }),
@@ -62,6 +67,56 @@ public class ClaseController {
                     .build();
         } else {
             return ResponseEntity.of(c.map(ClaseResponseDTO::of));
+        }
+    }
+
+    @Operation(summary = "Lista todas las clases")
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Listado de clases encontrado",
+                    content = { @Content(mediaType = "application/json",
+                    array = @ArraySchema(schema = @Schema(implementation = Clase.class)),
+                            examples = @ExampleObject(value = """
+                                    [
+                                         {
+                                            "id": 1,
+                                            "nombre": "2DAM",
+                                            "tutor": "Luis Miguel Lopez",
+                                            "numAportaciones" = 3,
+                                            "totalKilos" = 12.6
+                                        },
+                                        {
+                                            "id": 2,
+                                            "nombre": "1DAM",
+                                            "tutor": "Miguel Campos",
+                                            "numAportaciones" = 5,
+                                            "totalKilos" = 0.0
+                                        }
+                                    ]
+                                    """
+                            ))
+            }),
+            @ApiResponse(
+                    responseCode = "204",
+                    description = "No hay clases",
+                    content = @Content
+            )
+    })
+    @GetMapping("/")
+    public ResponseEntity<List<ClaseResponseDTO>> findAll() {
+        List<Clase> clases = service.findAll();
+
+        if (clases.isEmpty()) {
+            return ResponseEntity
+                    .status(HttpStatus.NO_CONTENT)
+                    .build();
+        } else {
+            return ResponseEntity.ok(
+                    clases.stream()
+                            .map(ClaseResponseDTO::of)
+                            .toList()
+            );
         }
     }
 }
