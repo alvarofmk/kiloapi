b97a3287da065955b44feec20dc1c21695c5f9c5
Antonio Montero <antoniomonforma99@gmail.com>
2022-12-15 14:29:31 +0100

    continue clase controller

--- src/main/java/com/salesianostriana/kilo/services/ClaseService.java
+++ src/main/java/com/salesianostriana/kilo/services/ClaseService.java
@@ -1,5 +1,6 @@
 package com.salesianostriana.kilo.services;
 
+import com.salesianostriana.kilo.dtos.requests.CreateClassDTO;
 import com.salesianostriana.kilo.entities.Clase;
 import com.salesianostriana.kilo.repositories.ClaseRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,5 +25,18 @@ public class ClaseService {
 
     public List<Clase> findAll(){
         return repository.findAll();
+    }
+
+
+    public Clase createClase(CreateClassDTO createClassDTO) {
+        Clase clase = repository.save(
+                Clase.builder()
+                        .tutor(createClassDTO.getTutor())
+                        .nombre(createClassDTO.getNombre())
+                        .aportaciones(null)
+                        .build()
+        );
+
+        return repository.save(clase);
     }
 }
