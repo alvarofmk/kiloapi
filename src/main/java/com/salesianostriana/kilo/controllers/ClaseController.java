20594e4ebdce33d5e1b958ff3fe9586bcf7b7d12
Antonio Montero <antoniomonforma99@gmail.com>
2022-12-15 09:27:01 +0100

    endpoint Get Clase by id and other problems fixed

--- src/main/java/com/salesianostriana/kilo/controllers/ClaseController.java
+++ src/main/java/com/salesianostriana/kilo/controllers/ClaseController.java
@@ -2,6 +2,12 @@ package com.salesianostriana.kilo.controllers;
 
 import com.salesianostriana.kilo.entities.Clase;
 import com.salesianostriana.kilo.services.ClaseService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,14 +18,35 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Optional;
 
+@RequiredArgsConstructor
 @RestController
 @RequestMapping("/clase")
 public class ClaseController {
 
-    @Autowired
-    private ClaseService service;
 
-    @GetMapping("/id")
+    private final ClaseService service;
+
+    @Operation(summary = "Lista una clase segun su Id")
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Clase por Id encontrada",
+                    content = { @Content(mediaType = "application/json",
+                    examples = @ExampleObject(value = """
+                        {
+                            "id" = 123,
+                            "nombre" = "2DAM",
+                            "tutor" = "Luis Miguel Lopez"
+                        }
+                    """))
+            }),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "No se encontró ninguna clase con el id Id indicado",
+                    content = @Content
+            )
+    })
+    @GetMapping("/{id}")
     public ResponseEntity<Clase> findById (@PathVariable Long id) {
         Optional<Clase> c = service.findById(id);
 
