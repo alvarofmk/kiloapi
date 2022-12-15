33a87d5aae6bb4562dfd3cd7a3fcdb1a8cca3df8
Carlos Jesús Durbán Viloca <carlosdurban91@gmail.com>
2022-12-15 14:21:42 +0100

    Endopoint terminado

--- src/main/java/com/salesianostriana/kilo/controllers/TipoAlimentoController.java
+++ src/main/java/com/salesianostriana/kilo/controllers/TipoAlimentoController.java
@@ -1,8 +1,19 @@
 package com.salesianostriana.kilo.controllers;
 
 
+import com.fasterxml.jackson.annotation.JsonView;
+import com.salesianostriana.kilo.dtos.TipoAlimentoDTO;
 import com.salesianostriana.kilo.entities.TipoAlimento;
 import com.salesianostriana.kilo.services.TipoAlimentoService;
+import com.salesianostriana.kilo.views.View;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,18 +26,52 @@ import java.util.List;
 @RestController
 @RequestMapping("tipoAlimento")
 @RequiredArgsConstructor
+@Tag(name = "Tipo de alimento", description = "Este es el controlador de los tipos de alimento")
 public class TipoAlimentoController {
 
     private final TipoAlimentoService tipoAlimentoService;
 
+    @Operation(summary = "Obtiene todos los tipos de alimento")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200",
+            description = "Se han encontrado tipos de alimento",
+            content = {
+                    @Content(mediaType = "application/json",
+                    array = @ArraySchema(schema = @Schema(implementation = TipoAlimentoDTO.class)),
+                    examples = {
+                            @ExampleObject(
+                                    value = """
+                                            [
+                                                {
+                                                    "id": 2,
+                                                    "nombre": "Pasta"
+                                                },
+                                                {
+                                                    "id": 3,
+                                                    "nombre": "Chocolate"
+                                                }
+                                            ]
+                                            """
+                            )
+                    })
+            }),
+            @ApiResponse(responseCode = "404",
+            description = "No se han encontrado tipos de alimentos",
+            content = @Content)
+    })
     @GetMapping("/")
-    public ResponseEntity<List<TipoAlimento>> getAllTipoAlimento() {
+    @JsonView(View.TipoAlimentoView.AllTipoAlimentoView.class)
+    public ResponseEntity<List<TipoAlimentoDTO>> getAllTipoAlimento() {
         List<TipoAlimento> lista = tipoAlimentoService.findAll();
+
+        List<TipoAlimentoDTO> resultado = lista.stream()
+                                                .map(ta -> TipoAlimentoDTO.of(ta))
+                                                .toList();
 
         if(lista.isEmpty()) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
         }else {
-            return ResponseEntity.status(HttpStatus.OK).body(lista);
+            return ResponseEntity.status(HttpStatus.OK).body(resultado);
         }
     }
 }
