a900c5789fd1f4bc5be62188a9bf756cb7253c47
Álvaro <alvarofmk@gmail.com>
2022-12-15 20:19:25 +0100

    Método terminado y documentado

--- src/main/java/com/salesianostriana/kilo/services/CajaService.java
+++ src/main/java/com/salesianostriana/kilo/services/CajaService.java
@@ -1,6 +1,7 @@
 package com.salesianostriana.kilo.services;
 
-import com.salesianostriana.kilo.dtos.requests.CreateCajaDTO;
+import com.salesianostriana.kilo.dtos.cajas.CreateCajaDTO;
+import com.salesianostriana.kilo.dtos.cajas.EditCajaDTO;
 import com.salesianostriana.kilo.entities.Caja;
 import com.salesianostriana.kilo.entities.KilosDisponibles;
 import com.salesianostriana.kilo.entities.Tiene;
@@ -9,10 +10,8 @@ import com.salesianostriana.kilo.entities.keys.TienePK;
 import com.salesianostriana.kilo.repositories.CajaRepository;
 import com.salesianostriana.kilo.repositories.TieneRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
-import javax.swing.text.html.Option;
 import java.util.List;
 import java.util.Optional;
 
@@ -43,6 +42,16 @@ public class CajaService {
         newCaja.setQr("http://localhost:8080/caja/" + newCaja.getId());
 
         return repository.save(newCaja);
+    }
+
+    public Optional<Caja> editCaja(EditCajaDTO editCajaDTO, Long id){
+        if(editCajaDTO.getNumero() <= 0)
+            return Optional.empty();
+        return repository.findById(id).map( cajaToEdit -> {
+            cajaToEdit.setQr(editCajaDTO.getQr());
+            cajaToEdit.setNumCaja(editCajaDTO.getNumero());
+            return repository.save(cajaToEdit);
+        });
     }
 
     public Optional<Caja> findById(Long id){
