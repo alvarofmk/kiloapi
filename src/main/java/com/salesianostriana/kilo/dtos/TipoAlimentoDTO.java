5c79ef18e3191bc2840832ba81364f075332f882
Carlos Jesús Durbán Viloca <carlosdurban91@gmail.com>
2022-12-16 13:55:28 +0100

    Endpoint POST Tipo alimento terminado

--- src/main/java/com/salesianostriana/kilo/dtos/TipoAlimentoDTO.java
+++ src/main/java/com/salesianostriana/kilo/dtos/TipoAlimentoDTO.java
@@ -1,14 +1,20 @@
 package com.salesianostriana.kilo.dtos;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonView;
+import com.salesianostriana.kilo.entities.DetalleAportacion;
 import com.salesianostriana.kilo.entities.TipoAlimento;
 import com.salesianostriana.kilo.views.View;
 import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 @AllArgsConstructor @NoArgsConstructor
 @Data
 @Builder
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class TipoAlimentoDTO {
 
     @JsonView({View.TipoAlimentoView.AllTipoAlimentoView.class,
@@ -21,6 +27,7 @@ public class TipoAlimentoDTO {
 
     @JsonView({View.TipoAlimentoView.TipoAlimentoByIdView.class})
     private double kilosDisponibles;
+
 
     public static TipoAlimentoDTO of(TipoAlimento t) {
         return TipoAlimentoDTO
--- src/main/java/com/salesianostriana/kilo/services/TipoAlimentoService.java
+++ src/main/java/com/salesianostriana/kilo/services/TipoAlimentoService.java
@@ -1,11 +1,15 @@
 package com.salesianostriana.kilo.services;
 
 
+import com.salesianostriana.kilo.dtos.TipoAlimentoDTO;
+import com.salesianostriana.kilo.entities.DetalleAportacion;
+import com.salesianostriana.kilo.entities.KilosDisponibles;
 import com.salesianostriana.kilo.entities.TipoAlimento;
 import com.salesianostriana.kilo.repositories.TipoAlimentoRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -26,6 +30,27 @@ public class TipoAlimentoService {
     public boolean existsById(Long id) {
         return tipoAlimentoRepository.existsById(id);
     }
+
+    public TipoAlimento createTipoAlimento(TipoAlimentoDTO dto) {
+
+        TipoAlimento creado = TipoAlimento
+                                        .builder()
+                                        .nombre(dto.getNombre())
+                                        .build();
+
+        creado.setKilosDisponibles(KilosDisponibles
+                                            .builder()
+                                            .id(creado.getId())
+                                            .tipoAlimento(creado)
+                                            .cantidadDisponible(0.0)
+                                            .build());
+
+        return tipoAlimentoRepository.save(creado);
+
+
+    }
+
+
 
 
 }
